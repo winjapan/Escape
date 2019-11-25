b128b890c7a5cfdd1c4f6e0acaa1bd2b47ad4b8a
カウピーTWICE大好き <gensikaku@gmail.com>
2019-11-25 23:50:23 +0900

    Hiden

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 5;
+    private Rigidbody rgbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        rgbody.AddForce(x * Speed,0, z* Speed);
+    }
+}

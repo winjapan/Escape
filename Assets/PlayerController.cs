e7e62aee3984287def90e8d2edbcb41e0c9d5c05
カウピーTWICE大好き <gensikaku@gmail.com>
2019-11-26 00:28:14 +0900

    sister

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,10 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     public float Speed = 5;
     private Rigidbody rgbody;
+
+    private Vector3 Player_Pos;
     // Start is called before the first frame update
     void Start()
     {
         rgbody = GetComponent<Rigidbody>();
+        Player_Pos = GetComponent<Transform>().position;
     }
 
     // Update is called once per frame
@@ -19,5 +22,16 @@ public class PlayerController : MonoBehaviour
         float z = Input.GetAxis("Vertical");
 
         rgbody.AddForce(x * Speed,0, z* Speed);
+
+        Vector3 diff = transform.position - Player_Pos;
+
+        if (diff.magnitude > 0.01f)
+        {
+
+            transform.rotation = Quaternion.LookRotation(diff);
+           
+        }
+       
+        Player_Pos = transform.position;
     }
 }
